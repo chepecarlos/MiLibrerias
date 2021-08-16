ef8127ad8f5b671e8b91a0f8c3d89b70ca37d9ff
chepecarlos <chepecarlos@alswblog.org>
2021-08-16 13:24:37 -0600

    Trasformando a submodulo

--- Demo.py
+++
@@ -1,7 +0,0 @@
-# from MisFunciones import *
-import MisFunciones
-
-logger = MisFunciones.ConfigurarLogging(__name__)
-
-print(f"Hola mundo {MisFunciones.ObtenerFolderConfig()}")
-logger.info("Prueba")
