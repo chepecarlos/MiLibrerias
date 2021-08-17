a40c5b58c40ab0801b3dcd6c04366ef59df5bed9
chepecarlos <chepecarlos@alswblog.org>
2021-08-17 15:35:54 -0600

    Cambiando nombre de ObtenerDato a ObtenerValor

--- FuncionesMQTT.py
+++ FuncionesMQTT.py
@@ -1,17 +1,17 @@
 import paho.mqtt.client as mqtt
 
 from .FuncionesLogging import ConfigurarLogging
-from .FuncionesArchivos import ObtenerDato
+from .FuncionesArchivos import ObtenerValor
 
 logger = ConfigurarLogging(__name__)
 
 
 def EnviarMensajeMQTT(Topic, Mensaje):
     """Envia un Mensaje Simple por MQTT."""
-    Usuario = ObtenerDato("/Data/MQTT.json", "Usuario")
-    Contrasenna = ObtenerDato("/Data/MQTT.json", "Contrasenna")
-    Servidor = ObtenerDato("/Data/MQTT.json", "Servidor")
-    Puesto = ObtenerDato("/Data/MQTT.json", "Puerto")
+    Usuario = ObtenerValor("/Data/MQTT.json", "Usuario")
+    Contrasenna = ObtenerValor("/Data/MQTT.json", "Contrasenna")
+    Servidor = ObtenerValor("/Data/MQTT.json", "Servidor")
+    Puesto = ObtenerValor("/Data/MQTT.json", "Puerto")
     MiMQTTSimple = mqtt.Client()
     MiMQTTSimple.username_pw_set(Usuario, Contrasenna)
     MiMQTTSimple.connect(Servidor, Puesto)
