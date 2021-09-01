70c26b1da897735a84e54d1121543cad11b3c8f9
chepecarlos <chepecarlos@alswblog.org>
2021-09-01 15:30:37 -0600

    Limpiar codigo de EnviarMensajeMQTT

--- FuncionesMQTT.py
+++ FuncionesMQTT.py
@@ -8,12 +8,13 @@ logger = ConfigurarLogging(__name__)
 
 def EnviarMensajeMQTT(Topic, Mensaje):
     """Envia un Mensaje Simple por MQTT."""
+    # TODO: Verificar que Existe Archivo MQTT.json
     Usuario = ObtenerValor("data/MQTT.json", "Usuario")
     Contrasenna = ObtenerValor("data/MQTT.json", "Contrasenna")
     Servidor = ObtenerValor("data/MQTT.json", "Servidor")
-    Puesto = ObtenerValor("data/MQTT.json", "Puerto")
+    Puerto = ObtenerValor("data/MQTT.json", "Puerto")
     MiMQTTSimple = mqtt.Client()
     MiMQTTSimple.username_pw_set(Usuario, Contrasenna)
-    MiMQTTSimple.connect(Servidor, Puesto)
+    MiMQTTSimple.connect(Servidor, Puerto)
     MiMQTTSimple.publish(Topic, Mensaje)
-    logger.info(f"Enviando por MQTT {Topic} - {Mensaje}")
+    logger.info(f"Enviando MQTT[{Topic}] {Mensaje}")
