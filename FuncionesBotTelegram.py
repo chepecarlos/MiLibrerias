3d0953889fe811d9ad4d6166e492cec105877483
chepecarlos <chepecarlos@alswblog.org>
2024-02-29 14:43:02 -0600

    actualizando libreria de telegram

--- FuncionesBotTelegram.py
+++ FuncionesBotTelegram.py
@@ -2,7 +2,6 @@
 
 """Librerías para mandar mensajes de telegram"""
 import asyncio
-
 import telegram
 
 from .FuncionesArchivos import ObtenerValor
@@ -17,10 +16,10 @@ def EnviarMensajeTelegram(mensaje, tokenBot=None, idChat=None):
         tokenBot = ObtenerValor("data/TelegramBot.json", "Token_Telegram")
     if idChat is None:
         idChat = ObtenerValor("data/TelegramBot.json", "ID_Chat")
-    
+
     if tokenBot is None or idChat is None:
         logger.error("No hay token de telegram")
         return
 
     bot = telegram.Bot(token=tokenBot)
-    asyncio.run(bot.send_message(chat_id=idChat, text=mensaje, parse_mode="HTML"))
+    asyncio.run(bot.send_message(chat_id=idChat, text=mensaje))
