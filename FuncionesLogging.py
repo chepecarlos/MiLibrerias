7733be3462dce028556456923d32bf5ef97940df
chepecarlos <chepecarlos@alsw.net>
2025-05-30 11:24:45 -0600

    mejorando nombre del programa y sistema de depuraccion

--- FuncionesLogging.py
+++ FuncionesLogging.py
@@ -1,4 +1,5 @@
-"""Libreria de logging."""
+"""Librería de logging."""
+
 import logging
 import os
 import sys
@@ -6,12 +7,25 @@ from datetime import datetime
 from pathlib import Path
 from logging import Logger
 
+import colorlog
+import inspect
+
+
+def nombrePrograma() -> str:
+    """Obtiene el nombre del paquete o módulo principal."""
+
+    frame = inspect.currentframe()
+    module = inspect.getmodule(frame.f_back)
+    if module and module.__package__:
+        return module.__package__.split(".")[0].lower()
+    elif module:
+        return module.__name__.split(".")[0].lower()
+    return "unknown"
+
 
 def FoldeLog() -> str:
-    """Devuelte ruta donde esta el folder de configuracion."""
-    Programa = os.path.basename(sys.argv[0]).lower()
-    Programa = os.path.splitext(Programa)[0]
-    Programa = str(Programa).split("_")[0]
+    """Devuelve ruta donde esta el folder de configuración."""
+    Programa = nombrePrograma()
 
     Folder = os.path.join(".config", Programa)
     Folder = os.path.join(Path.home(), Folder)
@@ -20,29 +34,49 @@ def FoldeLog() -> str:
     return Folder
 
 
-def ConfigurarLogging(Nombre, NivelLog=logging.INFO) -> Logger:
-    """Configura el archivo de depuración."""
-    #TODO: Nivel de depuracion
-    logger = logging.getLogger(Nombre)
+def ConfigurarLogging(
+    Nombre: str,
+    NivelLog: int = logging.WARNING
+    ) -> Logger:
+    """Configura el logger con salida en consola y archivo.
+
+    Args:
+        NivelLog (int): Nivel de logging (por defecto, logging.WARNING).
+
+    Returns:
+        Logger: Objeto logger configurado
+
+    """
+
+    if "--depuracion" in sys.argv:
+        NivelLog = logging.DEBUG
+    
+    # Atributos de logger https://docs.python.org/3/library/logging.html#logrecord-attributes
+    FormatoMensaje: str = "%(asctime)s %(process)d %(processName)s %(log_color)s%(module)s-%(funcName)s[%(levelname)s]%(reset)s: %(message)s"
+    
+    FormatoMensaje: str = "%(asctime)s %(process)d %(processName)s %(log_color)s%(module)s-%(funcName)s[%(levelname)s]%(reset)s: %(message)s"
+    FormatoTiempo: str = "%I:%M:%S %p"
+    
+    NombrePrograma = nombrePrograma()
+
+    logger = colorlog.getLogger(Nombre)
     logger.setLevel(NivelLog)
+    
 
-    ArchivoLog = FoldeLog()
+    loggerColor = colorlog.StreamHandler()
+    loggerColor.setFormatter(colorlog.ColoredFormatter(
+        FormatoMensaje, datefmt=FormatoTiempo))
+    logger.addHandler(loggerColor)
 
-    if not os.path.isdir(ArchivoLog):
-        os.makedirs(ArchivoLog)
+    ArchivoLog =  os.path.join(FoldeLog(), f"{NombrePrograma}.log")
 
-    # Agrega Fecha actua a log
-    ArchivoLog = ArchivoLog + "/{:%Y-%m-%d_%H:%M:%S}.log".format(datetime.now())
+    if not os.path.isdir(os.path.dirname(ArchivoLog)):
+        os.makedirs(os.path.dirname(ArchivoLog))
 
-    c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(ArchivoLog)
-    c_format = logging.Formatter("%(name)s-%(message)s")
-    f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-    c_handler.setFormatter(c_format)
-    f_handler.setFormatter(f_format)
-
-    logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
+    loggerArchivo = logging.FileHandler(ArchivoLog)
+    format_archivo = logging.Formatter(
+        "%(asctime)s %(process)d %(processName)s - %(name)s - %(levelname)s - %(message)s")
+    loggerArchivo.setFormatter(format_archivo)
+    logger.addHandler(loggerArchivo)
 
     return logger
